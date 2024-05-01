65fb27a573928f53da1ffd416d57f25e3112159f
Mahima <mahi87mnit@gmail.com>
2024-05-01 18:37:46 +0530

    api: add view and group api

--- books/views.py
+++ books/views.py
@@ -1,6 +1,22 @@
+from django.contrib.auth.models import Group, User
 from django.http import JsonResponse
+from rest_framework import permissions, viewsets
+
+from books.serializers import GroupSerializer, UserSerializer
 
 
 # Create your views here.
 def health(request):
     return JsonResponse({"success": True})
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
