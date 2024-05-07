5da20294ae4ce9bd4a5ddff10a6a60a55bee3ed6
Mahima <mahi87mnit@gmail.com>
2024-05-07 15:33:39 +0530

    feat: add pagination

--- bookstack/settings.py
+++ bookstack/settings.py
@@ -134,3 +134,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 DEFAULT_CURRENCY = "USD"
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
