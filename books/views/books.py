3ba686bcbdfe04ba06dc99ce95e9d710f18ff09d
Mahima <mahi87mnit@gmail.com>
2024-05-11 15:53:20 +0530

    feat: add authentication

--- books/views/books.py
+++ books/views/books.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import Http404
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,6 +10,8 @@ from books.serializers import BookSerializer
 
 
 class BooksView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request):
         data = json.loads(request.body)
         book_serializer = BookSerializer(data=data)
@@ -26,6 +29,8 @@ class BooksView(APIView):
 
 
 class BookView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request, pk):
         try:
             book = Book.objects.get(pk=pk)
