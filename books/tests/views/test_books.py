4cfd7897fe02d2d18ed4a8cf54e00e9fc38ea7bd
Mahima <mahi87mnit@gmail.com>
2024-05-11 17:57:20 +0530

    test: add test for booksView get request

--- books/tests/views/test_books.py
+++ books/tests/views/test_books.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APITestCase
 
+from books.models.book import Book
 from books.tests.models.test_book import make_book
 
 
@@ -46,6 +47,55 @@ class TestBooksView(APITestCase):
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {"title": ["This field is required."]})
+        self.client.logout()
+
+    def test_get_with_valid_request(self):
+        User.objects.create_user(**make_user())
+        book1 = make_book()
+        book2 = make_book(title="Fourth Wing")
+        book3 = make_book(title="Ram", author="Amish")
+        Book.objects.create(**book1)
+        Book.objects.create(**book2)
+        Book.objects.create(**book3)
+        self.client.login(username="mahi", password="1234")
+        response = self.client.get("/bookstack/books/", follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    "title": "Iron Flame",
+                    "author": "Rebbeca Yaroos",
+                    "publication_year": 2023,
+                    "genre": "Fiction",
+                    "language": "English",
+                },
+                {
+                    "title": "Fourth Wing",
+                    "author": "Rebbeca Yaroos",
+                    "publication_year": 2023,
+                    "genre": "Fiction",
+                    "language": "English",
+                },
+                {
+                    "title": "Ram",
+                    "author": "Amish",
+                    "publication_year": 2023,
+                    "genre": "Fiction",
+                    "language": "English",
+                },
+            ],
+        )
+        self.client.logout()
+
+    def test_get_should_not_let_user_in_without_login(self):
+        User.objects.create_user(**make_user())
+        Book.objects.create(**make_book())
+        response = self.client.get("/bookstack/books/", follow=True)
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(
+            response.json(), {"detail": "Authentication credentials were not provided."}
+        )
 
 
 def make_user(**kwargs):
