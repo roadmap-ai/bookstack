55f36de0ece444d5a555cec27b250e118789852d
Mahima <mahi87mnit@gmail.com>
2024-08-03 16:04:25 +0530

    encrypt and decrypt token for user interaction

--- social_auth/models.py
+++ social_auth/models.py
@@ -1,17 +1,27 @@
-import uuid
+import secrets
 
 from django.conf import settings
 from django.core.cache import cache
+from cryptography.fernet import Fernet
 
 
-# Create your models here.
 class Token:
     def create(self, user):
-        token = str(uuid.uuid4())
+        token = secrets.token_urlsafe(32)
         cache.set(
             key=f"token:{token}", value=user.id, timeout=settings.TOKEN_EXPIRY_SEC
         )
-        return token
 
-    def get_user_id(self, key):
-        return cache.get(f"token:{key}")
+        enc_key = settings.ENCRYPTION_KEY
+        f = Fernet(enc_key)
+        enc_token = f.encrypt(token.encode()).decode()
+
+        return enc_token
+
+    def get_user_id(self, enc_token):
+
+        enc_key = settings.ENCRYPTION_KEY
+        f = Fernet(enc_key)
+        token = f.decrypt(enc_token.encode()).decode()
+
+        return cache.get(f"token:{token}")
